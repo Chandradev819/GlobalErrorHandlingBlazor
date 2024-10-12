560112ee9162d105909cc3965e073b93dd797a0d
Chandradev Sah <chandradev819@gmail.com>
2024-10-12 22:09:57 +0530

    Add project files.

---
+++ GlobalError/Components/Layout/CustomErrorBoundary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace GlobalError.Components.Layout
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        ILogger<CustomErrorBoundary> Logger { get; set; } = default!;
+
+        protected override Task OnErrorAsync(Exception ex)
+        {
+            Logger.LogError(ex.Message, "Error message");
+            return Task.CompletedTask;
+        }
+    }
+}
